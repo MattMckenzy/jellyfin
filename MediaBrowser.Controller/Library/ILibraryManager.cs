a5237ec6dae89bf5bdc5d6540ea0d426fbbaae17
MattMckenzy <mattmckenzy@hotmail.com>
2022-12-05 19:02:56 -0500

    Skips folder scanning if empty.

--- MediaBrowser.Controller/Library/ILibraryManager.cs
+++ MediaBrowser.Controller/Library/ILibraryManager.cs
@@ -578,5 +578,7 @@ namespace MediaBrowser.Controller.Library
         /// This exists so plugins can trigger a library scan.
         /// </remarks>
         void QueueLibraryScan();
+
+        bool IsFolderEmpty(Folder folder, IDirectoryService directoryService = null);
     }
 }
